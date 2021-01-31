641f86305e128201f556bdfc268a1b9d3fa4b99b
Anisse Astier <anisse@astier.eu>
2021-01-31 19:16:20 +0100

    day10

---
+++ day10/src/main.rs
@@ -0,0 +1,79 @@
+use std::collections::HashMap;
+
+type Differences = HashMap<u8, usize>;
+
+fn main() {
+    let input = parse(include_str!("../input.txt"));
+    //part 1
+    println!("Part 1: {}", result_part1(&count_differences(&input)));
+    println!("Part 2: {}", result_part2(&input));
+}
+
+fn parse(input: &str) -> Vec<u32> {
+    input.lines()
+        .map(|l| l.trim().parse::<u32>().expect("Not an int"))
+        .collect()
+}
+
+fn count_differences(input: &[u32]) -> Differences {
+    let mut dif = Differences::new();
+    let mut sorted = Vec::from(input);
+    sorted.push(0);
+    sorted.sort_unstable();
+    sorted.iter().enumerate().skip(1).for_each(|(i, x)| *dif.entry((x - sorted[i-1]) as u8).or_insert(0) += 1);
+    *dif.entry(3).or_insert(0) += 1;
+    dif
+}
+
+fn result_part1(dif: &Differences) -> usize {
+    dif.get(&1).unwrap() * dif.get(&3).unwrap()
+}
+
+// Simple memoized recursion
+fn walk_chains_recur(input: &[u32], cache: &mut [u64]) -> u64 {
+    if input.is_empty() {
+        return 0;
+    }
+    if input.len() == 1 {
+        return 1
+    }
+    if cache[0] > 0 {
+        return cache[0]
+    }
+    let mut total = 0;
+    for i in 1..=3 {
+        if i >= input.len() {
+            break
+        }
+        if input[i] - input[0] > 3 {
+            break
+        }
+        total += walk_chains_recur(&input[i..input.len()], &mut cache[i..input.len()])
+    }
+    cache[0] = total;
+    total
+}
+
+fn result_part2(input: &[u32]) -> u64 {
+    let mut sorted = Vec::from(input);
+    sorted.push(0);
+    sorted.sort_unstable();
+    sorted.push(sorted.last().unwrap() + 3);
+    let mut cache = vec!(0; sorted.len());
+    walk_chains_recur(&sorted, &mut cache)
+}
+
+#[test]
+fn test() {
+    let input = parse(include_str!("../sample.txt"));
+    let input2 = parse(include_str!("../sample2.txt"));
+    let dif1 = count_differences(&input);
+    let dif2 = count_differences(&input2);
+    assert_eq!(dif1.get(&1), Some(&7));
+    assert_eq!(dif1.get(&3), Some(&5));
+    assert_eq!(dif2.get(&1), Some(&22));
+    assert_eq!(dif2.get(&3), Some(&10));
+
+    assert_eq!(result_part2(&input), 8);
+    assert_eq!(result_part2(&input2), 19208);
+}
