e7f8db735163b8d69c58d9c0ed6c68da0492a496
Anisse Astier <anisse@astier.eu>
2021-02-07 17:27:05 +0100

    day11: part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -10,7 +10,9 @@ type LayoutSlice = [Vec<Tile>];
 
 fn main() {
     let input = parse(include_str!("../input.txt"));
-    println!("Empty seats after stabilization: {}", result_part1(&input));
+    println!("Part 1 empty seats after stabilization: {}", result(&input, iteration_part1));
+
+    println!("Part 2 empty seats after stabilization: {}", result(&input, iteration_part2));
 }
 
 fn parse(input: &str) -> Layout {
@@ -29,7 +31,7 @@ fn parse(input: &str) -> Layout {
         .collect()
 }
 
-fn result_part1(l: &LayoutSlice) -> usize {
+fn result(l: &LayoutSlice, iteration: fn(&LayoutSlice) -> Layout) -> usize {
     let mut prev = l.to_owned();
     let mut current;
     loop {
@@ -38,7 +40,7 @@ fn result_part1(l: &LayoutSlice) -> usize {
             break;
         }
         prev = current;
-    };
+    }
     // count occupied
     current
         .iter()
@@ -47,18 +49,26 @@ fn result_part1(l: &LayoutSlice) -> usize {
         .count()
 }
 
-fn iteration(l: &LayoutSlice) -> Layout {
+fn iteration_part1(l: &LayoutSlice) -> Layout {
+    iteration(l, count_adjacent_occupied, 4)
+}
+
+fn iteration_part2(l: &LayoutSlice) -> Layout {
+    iteration(l, count_sightline_occupied, 5)
+}
+
+fn iteration(l: &LayoutSlice, count: fn(&LayoutSlice, usize, usize) -> u8, max: u8) -> Layout {
     l.iter()
         .enumerate()
         .map(|(i, r)| {
             r.iter()
                 .enumerate()
                 .map(|(j, &t)| {
-                    let adj = count_adjacent_occupied(l, i, j);
-                    if t == Tile::OccupiedSeat && adj >= 4 {
+                    let seen = count(l, i, j);
+                    if t == Tile::OccupiedSeat && seen >= max {
                         return Tile::EmptySeat;
                     }
-                    if t == Tile::EmptySeat && adj == 0 {
+                    if t == Tile::EmptySeat && seen == 0 {
                         return Tile::OccupiedSeat;
                     }
                     t
@@ -89,6 +99,37 @@ fn count_adjacent_occupied(l: &LayoutSlice, i: usize, j: usize) -> u8 {
     count
 }
 
+fn count_sightline_occupied(l: &LayoutSlice, i: usize, j: usize) -> u8 {
+    let mut count = 0;
+    let ilen = l.len() as isize;
+    let jlen = l[0].len() as isize;
+    for iinc in -1..=1 {
+        for jinc in -1..=1 {
+            let mut ipos = i as isize;
+            let mut jpos = j as isize;
+            if iinc == 0 && jinc == 0 {
+                continue
+            }
+            loop {
+                ipos += iinc;
+                jpos += jinc;
+                if ipos < 0 || ipos >= ilen || jpos < 0 || jpos >= jlen {
+                    break;
+                }
+                match l[ipos as usize][jpos as usize] {
+                    Tile::OccupiedSeat => {
+                        count += 1;
+                        break;
+                    }
+                    Tile::EmptySeat => break,
+                    Tile::Floor => {}
+                }
+            }
+        }
+    }
+    count
+}
+
 fn cmp(l1: &LayoutSlice, l2: &LayoutSlice) -> bool {
     l1.iter()
         .zip(l2)
@@ -102,13 +143,30 @@ fn test() {
     assert!(cmp(&input, &input));
     let input1 = parse(include_str!("../sample2.txt"));
     let input2 = parse(include_str!("../sample3.txt"));
-    let it0 = iteration(&input);
+    let it0 = iteration_part1(&input);
     assert!(!cmp(&input, &it0));
     assert_eq!(it0[0][2], Tile::OccupiedSeat);
     assert_eq!(count_adjacent_occupied(&it0, 0, 2), 4);
-    let it1 = iteration(&it0);
-    let it2 = iteration(&iteration(&iteration(&it1)));
+    let it1 = iteration_part1(&it0);
+    let it2 = iteration_part1(&iteration_part1(&iteration_part1(&it1)));
     assert!(cmp(&input1, &it1));
     assert!(cmp(&input2, &it2));
-    assert_eq!(result_part1(&input), 37);
+    assert_eq!(result(&input, iteration_part1), 37);
+
+    //part 2
+    assert_eq!(
+        count_sightline_occupied(&parse(include_str!("../sample4.txt")), 4, 3),
+        8
+    );
+    assert_eq!(
+        count_sightline_occupied(&parse(include_str!("../sample5.txt")), 1, 1),
+        0
+    );
+    assert_eq!(
+        count_sightline_occupied(&parse(include_str!("../sample6.txt")), 3, 3),
+        0
+    );
+
+    assert!(cmp(&parse(include_str!("../sample7.txt")), &iteration_part2(&iteration_part2(&input))));
+    assert_eq!(result(&input, iteration_part2), 26);
 }
